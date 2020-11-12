cf22f3073eaed6535e0e7f14e9b681506f47a5a1
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-11-12 23:08:05 +0300

    Adds Html skeleton to the project

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function getBookDetails() {
+  console.log(document.getElementById('book'));
+}
+function addBookToLibrary() {
+  // do stuff here
+}
