4505b1a07443e2679deb78e2daf24ab0a8c7032b
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-12 23:53:30 +0300

    REMOVE test codes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 const myLibrary = [];
 
-
-
-
 function Book(title, author, pages) {
   // the constructor...
   this.title = title;
@@ -11,24 +8,8 @@ function Book(title, author, pages) {
 }
 
 function getBookDetails() {
-  // console.log(document.getElementById('book').value);
-  let value = document.getElementById('book').value
-  myLibrary.push(value)
-  displayAllBooks()
-
-  // let totalBooks = document.getElementById('totalBooks')
-  // totalBooks.innerText += value
+  
 }
 function addBookToLibrary() {
   // do stuff here
-}
-
-displayAllBooks()
-
-function displayAllBooks() {
-  let totalBooks = document.getElementById('totalBooks')
-  // totalBooks.innerText = 'some text'
-  for (let i = 0; i < myLibrary.length; i++) {
-    totalBooks.innerText += myLibrary[i]
-  }
 }
