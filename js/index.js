644496c0601455fe38bf1e1f29860b00c0458258
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-12 23:52:24 +0300

    ADD test code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 
+
+
+
 function Book(title, author, pages) {
   // the constructor...
   this.title = title;
@@ -8,8 +11,24 @@ function Book(title, author, pages) {
 }
 
 function getBookDetails() {
-  console.log(document.getElementById('book'));
+  // console.log(document.getElementById('book').value);
+  let value = document.getElementById('book').value
+  myLibrary.push(value)
+  displayAllBooks()
+
+  // let totalBooks = document.getElementById('totalBooks')
+  // totalBooks.innerText += value
 }
 function addBookToLibrary() {
   // do stuff here
+}
+
+displayAllBooks()
+
+function displayAllBooks() {
+  let totalBooks = document.getElementById('totalBooks')
+  // totalBooks.innerText = 'some text'
+  for (let i = 0; i < myLibrary.length; i++) {
+    totalBooks.innerText += myLibrary[i]
+  }
 }
